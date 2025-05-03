4b5fb782c300177d48f7ef2db5aede4c428738c8
guboc11 <guboc11@gmail.com>
2025-05-03 16:17:27 +0900

    Update button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,10 +10,10 @@ function App() {
         <div>Daily Tooth Brushing</div>
         {/* <div className="flex border-2 border-black"> */}
         <div className="flex min-h-45 w-full border-2 border-black items-center justify-around">
-          <Button className="w-45 border-2 border-black cursor-pointer">
+          <Button className="hover:bg-amber-200 active:bg-amber-300 w-45 border-2 border-black cursor-pointer">
             Yes
           </Button>
-          <Button className="w-45 border-2 border-black cursor-pointer">
+          <Button className="hover:bg-amber-200 active:bg-amber-300 w-45 border-2 border-black cursor-pointer">
             No
           </Button>
         </div>
