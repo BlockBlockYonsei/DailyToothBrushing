e8b05160e63f76fbc0a9d34505acca541691a5d2
guboc11 <guboc11@gmail.com>
2025-05-03 16:21:13 +0900

    Add Wallet Button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,47 @@
 import { useState } from "react";
 import { Button } from "./components/ui/button";
+import {
+  useConnectWallet,
+  useCurrentAccount,
+  useDisconnectWallet,
+  useWallets,
+} from "@mysten/dapp-kit";
+import { useLocation } from "react-router-dom";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const account = useCurrentAccount();
+  const wallets = useWallets();
+  const location = useLocation();
+
+  const { mutate: disconnect } = useDisconnectWallet();
+  const { mutate: connect } = useConnectWallet();
 
   return (
     <>
       <div>
+        <nav>
+          {account ? (
+            <div className="flex flex-col items-center">
+              <p className="text-center text-lg font-bold">{account.label}</p>
+              <Button
+                className="w-45 bg-black text-white hover:bg-white hover:bg-blue-200"
+                onClick={() => disconnect()}
+              >
+                Disconnect
+              </Button>
+            </div>
+          ) : (
+            <div className="mx-auto w-5/6">
+              <Button
+                className="w-45 border-2 border-black bg-white text-black hover:bg-blue-200"
+                onClick={() => connect({ wallet: wallets[0] })}
+              >
+                Connect wallet
+              </Button>
+            </div>
+          )}
+        </nav>
         <div>Daily Tooth Brushing</div>
         {/* <div className="flex border-2 border-black"> */}
         <div className="flex min-h-45 w-full border-2 border-black items-center justify-around">
